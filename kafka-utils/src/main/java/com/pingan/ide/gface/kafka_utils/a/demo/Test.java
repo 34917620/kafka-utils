f997b2c7160e8d9d0e5c2a47dc9272e7491008dd
Administrator <Administrator@PSWGO3KGHWVK1QZ.mshome.net>
2019-02-28 11:24:12 +0800

    zzzzz

--- kafka-utils/src/main/java/com/pingan/ide/gface/kafka_utils/a/demo/Test.java
+++ kafka-utils/src/main/java/com/pingan/ide/gface/kafka_utils/a/demo/Test.java
@@ -9,7 +9,7 @@ package com.pingan.ide.gface.kafka_utils.a.demo;
  */
 public class Test {
    public void dosome2(){
-      System.out.println("ddddd");
+      System.out.println("zzzzz");
 
    }
 }
